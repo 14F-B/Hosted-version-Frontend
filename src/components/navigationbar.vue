aee1ebd334387fba95a4992ab00e54d2236aaa4d
CsizmaziaAdam <113601467+CsizmaziaAdam@users.noreply.github.com>
2023-04-19 12:46:52 +0200

    mobile

--- src/components/navigationbar.vue
+++ src/components/navigationbar.vue
@@ -1,75 +1,73 @@
 <template>
 
-    <div id="header" class="fixed-top d-flex align-items-cente">
-      
-      <div class="container-fluid container-xl d-flex flex-row">
-        <a href="/" class="logo me-auto me-lg-0"><img id="logo" src="../assets/img/logo.png" alt="" class=""></a>
-
-        <nav id="navbar" class="navbar order-last order-lg-0">
-          <ul>
-            <li><a class="nav-link scrollto active" href="/#welcome-screen" >Home</a></li>
-            <li><a class="nav-link scrollto" href="/#about" >Ismertető</a></li>
-            <li><a class="nav-link scrollto" href="/#actually" >Programok</a></li>
-            <li><a class="nav-link scrollto" href="/#media">Média</a></li>
-            <li><a class="nav-link scrollto" href="/#map">Helyszín</a></li>
-            <li><a class="nav-link scrollto" href="/#contact">Kapcsolat</a></li>
-        
-            <div v-if="!isLoggedIn" class="navbar-buttons m-3">
-                <label class="modal-btn" for="modal-toggle">Bejelentkezés/<br>Regisztráció</label>
-            </div>
-            <div v-if="isLoggedIn" class="navbar-buttons m-3 ">
-            <form action="/userpage">
-              <button class="logoutbtn navbar-buttons" type="submit">Fiók</button>
-            </form> 
-            </div>
-            <div v-if="isLoggedIn" class="navbar-buttons m-3">
-            <form @click="logout">
-              <button class="logoutbtn navbar-buttons" type="submit">Kijelentkezés</button>
-            </form> 
-            </div>
-          </ul>
-          <i class="bi bi-list mobile-nav-toggle"></i>
-        </nav>
-      </div>
-    </div>
-
+  <div id="header" class="fixed-top d-flex align-items-cente">
     
+    <div class="container-fluid container-xl d-flex flex-row">
+      <a href="/" class="logo me-auto me-lg-0"><img id="logo" src="../assets/img/logo.png" alt="" class=""></a>
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a class="nav-link scrollto active" href="/#welcome-screen" >Home</a></li>
+          <li><a class="nav-link scrollto" href="/#about" >Ismertető</a></li>
+          <li><a class="nav-link scrollto" href="/#actually" >Programok</a></li>
+          <li><a class="nav-link scrollto" href="/#media">Média</a></li>
+          <li><a class="nav-link scrollto" href="/#map">Helyszín</a></li>
+          <li><a class="nav-link scrollto" href="/#contact">Kapcsolat</a></li>
+      
+          <div v-if="!isLoggedIn" class="navbar-buttons m-3">
+              <label class="modal-btn" for="modal-toggle">Bejelentkezés/<br>Regisztráció</label>
+          </div>
+          <div v-if="isLoggedIn" class="navbar-buttons m-3 ">
+          <form action="/userpage">
+            <button class="logoutbtn navbar-buttons" type="submit">Fiók</button>
+          </form> 
+          </div>
+          <div v-if="isLoggedIn" class="navbar-buttons m-3">
+          <form @click="logout">
+            <button class="logoutbtn navbar-buttons" type="submit">Kijelentkezés</button>
+          </form> 
+          </div>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav>
+    </div>
+  </div>
+
+  
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
-// import { on, select } from '../assets/js/main.js'
+import { on, select } from '../assets/js/main.js'
 
 
 export default {
-  computed: {
-    ...mapGetters([
-      'getEmail'
-    ]),
-    isLoggedIn() {
-      return this.getEmail !== ''
+computed: {
+  ...mapGetters([
+    'getEmail'
+  ]),
+  isLoggedIn() {
+    return this.getEmail !== ''
+  }
+},
+methods: {
+  ...mapActions([
+    'logout'
+  ]),
+  handleClick() {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.$refs.toggleBtn.classList.toggle('bi-list')
+    this.$refs.toggleBtn.classList.toggle('bi-x')
+  }
+},
+mounted() {
+  on('click', '.mobile-nav-toggle', this.handleClick)
+  on('click', '.navbar .dropdown > a', function(e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
     }
-  },
-  methods: {
-    ...mapActions([
-      'logout'
-    ])
-  },
-  // mounted() {
-  //   on('click', '.mobile-nav-toggle', function(e) {
-  //     select('#navbar').classList.toggle('navbar-mobile')
-  //     this.classList.toggle('bi-list')
-  //     this.classList.toggle('bi-x')
-  //   })
-
-  //   on('click', '.navbar .dropdown > a', function(e) {
-  //   if (select('#navbar').classList.contains('navbar-mobile')) {
-  //     e.preventDefault()
-  //     this.nextElementSibling.classList.toggle('dropdown-active')
-  //   }
-  // }, true)
-  // }
+  }, true)
+}
 }
 </script>
-
-
